2b1483d84c058d526a6fa9a056f42b9b2036f21c
itsmesrihere-dot <itsmesrihere@gmail.com>
2026-08-17 19:24:57 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,12 @@
+num1 = int(input("enter the num:"))
+num2 = int(input("enter the num:"))
+op =input("enter the operator:")
+if op=="+":
+    print(num1+num2)
+elif op=="-":
+    print(num1-num2)
+elif op=="*":
+    print(num1*num2)
+elif op=="/":
+    print(num1/num2)
+    
